bd99e5294dd2b8c586a913ca4948597b4ebe4aa4
SinemAkkoyun <125818196+Cisii@users.noreply.github.com>
2024-01-08 17:26:33 +0300

    MyBlogerSite

---
+++ TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        Context ctx = new Context();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(iletisim model)
+        {
+            ctx.iletisims.Add(model);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
